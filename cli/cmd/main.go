1eda0cbd205b37d07142361f1bd569e99edf628e
alex <zsasha957@gmail.com>
2026-01-31 21:58:17 +0300

    taskfile

--- cli/cmd/main.go
+++ cli/cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"sync"
 	"time"
+
+	probing "github.com/prometheus-community/pro-bing"
 )
 
 func main() {
@@ -111,13 +115,42 @@ func main() {
 }
 
 func getIPRange(localip string) []string {
-	ip := net.ParseIP(localip).To4()
+	ip := net.ParseIP(localip)
+	ipv4 := ip.To4()
+
 	res := []string{}
+	var wg sync.WaitGroup
+	resChan := make(chan string, 255)
+
 	for i := 1; i <= 255; i++ {
-		res = append(res, fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], i))
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			nIP := fmt.Sprintf("%d.%d.%d.%d", ipv4[0], ipv4[1], ipv4[2], i)
+			pinger := probing.New(nIP)
+			pinger.Count = 1
+
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+			defer cancel()
+
+			if err := pinger.RunWithContext(ctx); err == nil && pinger.PacketsRecv > 0 {
+				resChan <- nIP
+			}
+		}(i)
 	}
+
+	go func() {
+		wg.Wait()
+		close(resChan)
+	}()
+
+	for ip := range resChan {
+		res = append(res, ip)
+	}
+
 	return res
 }
+
 
 func getLocalIP() (string, error) {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
